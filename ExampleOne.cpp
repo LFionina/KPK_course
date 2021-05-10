b171b92b3e5e319d939029806ae39d0fca0ef871
LFionina <80356955+LFionina@users.noreply.github.com>
2021-05-10 19:05:02 +0400

    Файл с примером 1
    Добавлен файл с примером 1 рисования елки

---
+++ ExampleOne.cpp
@@ -0,0 +1,22 @@
+# include "LFioninaLib.h"
+# include "TXLib.h"
+# include <stdlib.h>
+
+int main()
+    {
+    txCreateWindow (1200, 700);
+    SubTitlesDraw ( 50, 20, 40, "Arial", TX_WHITE, "ПРИМЕР 1: Рисование елки с разными параметрами");
+
+    TreeOneDraw   (100, 100, 1, 0, 30, 30, 2,  50, 20);
+    SubTitlesDraw ( 50, 200, 20, "Arial", TX_WHITE, "Елка: размер 1, смещение верха 0, ширина 30, высота 30");
+
+    TreeOneDraw   (100, 300, 1, 20, 70, 50, 2,  50, 20);
+    SubTitlesDraw ( 50, 500, 20, "Arial", TX_WHITE, "Елка: размер 1, смещение верха +20, ширина 70, высота 50");
+
+    TreeOneDraw   (600, 100, 0.5, -10, 60, 30, 2,  50, 120);
+    SubTitlesDraw (550, 200, 20, "Arial", TX_WHITE, "Елка: размер 0.5, смещение верха -10, ширина 60, высота 30");
+
+    TreeOneDraw   (600, 300, 0.8, 0, 60, 40, 2,  170, 180);
+    SubTitlesDraw (550, 500, 20, "Arial", TX_WHITE, "Елка: размер 0.8, смещение верха 0, ширина 60, высота 40");
+    return 0;
+    }
